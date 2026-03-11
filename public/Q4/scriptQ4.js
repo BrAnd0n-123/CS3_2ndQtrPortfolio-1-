c5df642e462f52bcaeec26abe375b11f1f721c2c
BrAnd0n-123 <b2029btpfernando@pshs.edu.ph>
2026-03-11 22:33:28 +0800

    Suddenly, a tempest.

--- public/Q4/scriptQ4.js
+++ public/Q4/scriptQ4.js
@@ -1,26 +1,31 @@
 let savedMovies = JSON.parse(localStorage.getItem("savedMovies")) || {}; //Start up a new object to hold all information
 
-          function displayMovie() {
-            const list = document.getElementById("movieList");
-            list.innerHTML = "";
+            function displayMovie() {
+                const list = document.getElementById("movieList");
+                list.innerHTML = "";
 
-            for (let i in savedMovies) {
-                const info = savedMovies[i];
-                const div = document.createElement("div");
-                div.className = "infoSavedMovie"; 
-                
-                // Use structured HTML for easier styling
-                div.innerHTML = `
-                    <div class="movieInfoText">
-                        <span class="savedTitle">${info.title}</span>
-                        <span class="savedYear">(${info.year})</span>
-                        <div class="savedDetails">${info.genre} | Rating: ${"★".repeat(info.stars)}</div>
-                    </div>
-                    <button class="deleteBtn" onclick="deleteMovie('${i}')">DELETE</button>
-                `;
-                list.appendChild(div);
+                for (let i in savedMovies) {
+                    const info = savedMovies[i];
+
+                    const ratingsArray = info.ratings || [Number(info.stars) || 0];
+                    const avgStars = Math.ceil(
+                        ratingsArray.reduce((sum, s) => sum + s, 0) / ratingsArray.length
+                    );
+
+                    const div = document.createElement("div");
+                    div.className = "infoSavedMovie"; 
+
+                    div.innerHTML = `
+                        <div class="movieInfoText">
+                            <span class="savedTitle">${info.title}</span>
+                            <span class="savedYear">(${info.year})</span>
+                            <div class="savedDetails">${info.genre} | Rating: ${"★".repeat(avgStars)}</div>
+                        </div>
+                        <button class="deleteBtn" onclick="deleteMovie('${i}')">DELETE</button>
+                    `;
+                    list.appendChild(div);
+                }
             }
-        }
 
           document.getElementById("formMovie").addEventListener("submit", function(event) {
 
@@ -32,7 +37,11 @@ let savedMovies = JSON.parse(localStorage.getItem("savedMovies")) || {}; //Start
           const stars = document.querySelector('input[name="movieStars"]:checked')?.value;
           const key = title.toLowerCase();
 
-          savedMovies[key] = {title, year, genre, stars};
+          if (savedMovies[key]) {
+                savedMovies[key].ratings.push(Number(stars));
+            } else {
+                savedMovies[key] = {title, year, genre, ratings: [Number(stars)]};
+            }
           localStorage.setItem("savedMovies", JSON.stringify(savedMovies));
           displayMovie();
           this.reset();
