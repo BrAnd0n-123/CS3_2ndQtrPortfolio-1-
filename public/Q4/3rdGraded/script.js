71df79de034455b1fe8ea6ee0a032a715739b06a
BrAnd0n-123 <b2029btpfernando@pshs.edu.ph>
2026-03-30 09:51:46 +0800

    commit

--- public/Q4/3rdGraded/script.js
+++ public/Q4/3rdGraded/script.js
@@ -1,6 +1,6 @@
-function plotPoint(x0, y0, x, y) {  //CHANGE 2: DECLARE x0 x y0 y
+function plotPoint(x0, y0, x, y) {  //CHANGE 7: DECLARE x0 x y0 y
 
-    let in1 = document.getElementById("in1"); //CHANGE 3: DECALRE THESE THINGIES TOO
+    let in1 = document.getElementById("in1"); //CHANGE 8: DECALRE THESE THINGIES TOO
     let out1 = document.getElementById("out1");
 
     console.log(x0, y0, x, y) // checks the arguments passed to this function
@@ -9,10 +9,10 @@ function plotPoint(x0, y0, x, y) {  //CHANGE 2: DECLARE x0 x y0 y
 
     var point = document.createElement('div');  
     point.className = 'point';
-    point.style.position = "absolute"; /* CHANGE 4: USE PROPER STYLE ACCORDING TO COMMENT --
+    point.style.position = "absolute"; /* CHANGE 9: USE PROPER STYLE ACCORDING TO COMMENT --
     control where to place the div on the screen using left and bottom of position:absolute */
 
-    //CHANGE 5: FIX STYLING BY ADDING .
+    //CHANGE 10: FIX STYLING BY ADDING .
     point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
     point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
     document.getElementById('coordinatePlane').appendChild(point);
@@ -32,4 +32,4 @@ function plotPoint(x0, y0, x, y) {  //CHANGE 2: DECLARE x0 x y0 y
     else{
       out1.innerHTML = "divisa"
     }
-  } //CHANGE 1: Function plotPoint is not closed
+  } //CHANGE 6: Function plotPoint is not closed
